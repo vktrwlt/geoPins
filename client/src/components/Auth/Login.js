d2c1976527aa9b86d71e5c956c9d522d79bf94f8
Victor Tsang <Victorwltsang@gmail.com>
2020-04-16 22:29:31 -0700

    finished chapter 3

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { GraphQLClient } from "graphql-request";
+import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `{
+  me{
+    _id
+    email
+    name
+    picture
+  }
+}`;
+
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = async (googleUser) => {
+    // user token
+    console.log(googleUser);
+    const idToken = googleUser.getAuthResponse().id_token;
+    const client = new GraphQLClient("http://localhost:4000/graphql", {
+      headers: {
+        authorization: idToken,
+      },
+    });
+    const data = await client.request(ME_QUERY);
+    console.log(data);
+  };
+  return (
+    <GoogleLogin
+      clientId="332037095951-r6umeh8ptp8aviiec1o2jkaodcaemqv7.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+  );
 };
 
 const styles = {
@@ -12,8 +41,8 @@ const styles = {
     display: "flex",
     justifyContent: "center",
     flexDirection: "column",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 };
 
 export default withStyles(styles)(Login);
--- client/src/pages/Splash.js
+++ client/src/pages/Splash.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import Login from "../components/Auth/Login";
 const Splash = () => {
-  return <div>Splash Page</div>;
+  return <Login />;
 };
 
 export default Splash;
