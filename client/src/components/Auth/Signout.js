5feb029a31d60a3cfabc05012ba852bc296a0255
Victor Tsang <Victorwltsang@gmail.com>
2020-05-08 08:54:21 -0700

    header

--- client/src/components/Auth/Signout.js
+++ client/src/components/Auth/Signout.js
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import ExitToApp from "@material-ui/icons/ExitToApp";
-// import Typography from "@material-ui/core/Typography";
+import { GoogleLogout } from "react-google-login";
+import ExitToApp from "@material-ui/icons/ExitToApp";
+import Typography from "@material-ui/core/Typography";
+import Context from "../../context";
 
 const Signout = ({ classes }) => {
-  return <div>Signout</div>;
+  const { dispatch } = useContext(Context);
+  const onSignout = () => {
+    dispatch({
+      type: "SIGN_OUT_USER",
+    });
+    console.log("Sign out user");
+  };
+  return (
+    <GoogleLogout
+      onLogoutSuccess={onSignout}
+      render={({ onClick }) => (
+        <span className={classes.root} onClick={onClick}>
+          <Typography variant="body1" className={classes.buttonText}>
+            Sign out
+          </Typography>
+          <ExitToApp className={classes.buttonIcon} />
+        </span>
+      )}
+    />
+  );
 };
 
 const styles = {
   root: {
     cursor: "pointer",
-    display: "flex"
+    display: "flex",
   },
   buttonText: {
-    color: "orange"
+    color: "orange",
   },
   buttonIcon: {
     marginLeft: "5px",
-    color: "orange"
-  }
+    color: "orange",
+  },
 };
 
 export default withStyles(styles)(Signout);
